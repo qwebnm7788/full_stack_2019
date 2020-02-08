df5378b4c6606ca9f485c63d935d0fc8e52b8495
qwebnm7788 <qwebnm7788@naver.com>
2020-02-08 18:08:05 +0900

    part 1c done

--- part1/exercise2/src/index.js
+++ part1/exercise2/src/index.js
@@ -1,38 +1,32 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = ({ name, age }) => {
-    const bornYear = () => new Date().getFullYear() - age
-
-    return (
-        <div>
-            <p>
-                Hello {name}, you are {age} years old
-            </p>
-            <p>So you were probably born in {bornYear()}</p>
-        </div>
-    )
-}
+const Display = ({counter}) => <div>{counter}</div>
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
 
 const App = (props) => {
     const [ counter, setCounter ] = useState(0)
-
-    setTimeout(
-        () => setCounter(counter + 1),
-        1000
-    )
-
-    console.log("rendering...", counter)
-
-    const name = "Jaewon"
-    const age = 26
+    const setToValue = (value) => setCounter(value)
 
     return (
         <div>
-            <h1>Greetings</h1>
-            <Hello name="Choi" age={26 + 1} />
-            <Hello name={name} age={age} />
-            <div>{counter}</div>
+            <Display counter={counter} />
+            <Button
+                onClick={() => setToValue(counter + 1)}
+                text='plus'
+            />
+            <Button
+                onClick={() => setToValue(counter - 1)}
+                text='minus'
+            />
+            <Button
+                onClick={() => setToValue(0)}
+                text='zero'
+            />
         </div>
     )
 }
