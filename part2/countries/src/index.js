e7fd705d74b2cc582c99394c13027aec6aa30662
qwebnm7788 <qwebnm7788@naver.com>
2020-03-03 23:40:14 +0900

    part2 exercise step2 done..?

--- part2/countries/src/index.js
+++ part2/countries/src/index.js
@@ -2,19 +2,65 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+const Country = ({ country }) => {
+    return (
+        <div>
+            <p>{country.name}</p>
+        </div>
+    )
+}
+
+const CountryDetail = ({ country }) => {
+    const rows = () => country.languages.map(language => <li>{language.name}</li>);
+    return (
+        <div>
+            <h2>{country.name}</h2>
+            <p>Capital {country.capital}</p>
+            <p>Population {country.population}</p>
+            <h3>Languages</h3>
+            <ul>{rows()}</ul>
+            <img src={country.flag} />
+        </div>
+    )
+}
+
 const Result = ( props ) => {
-    const filteredCountries = props.countries.filter(country => country['name'].startsWith(props.keyword));
-    if (filteredCountries.length === 1 ) {
+    const filteredCountries = props.countries.filter(country => {
+        return country['name'].startsWith(props.keyword);
+    });
+    const [selected, setSelected] = useState(null);
+
+    if (filteredCountries.length === 1) {
         return (
             <div>
-                
+                <CountryDetail country={filteredCountries[0]} />
+            </div>
+        )
+    } else if (selected != null) {
+        return (
+            <div>
+                <CountryDetail country={selected} />
             </div>
         )
     } else if (filteredCountries.length < 10) {
-        for (var i = 0; i < filteredCountries.length; i++) {
-        }
+        const rows = () => filteredCountries.map(country => {
+            return (
+                <div>
+                    <Country country={country} /><button onClick={() => setSelected(country)}>show</button>
+                </div>
+            )
+        });
+        return (
+            <div>
+                {rows()}
+            </div>
+        )
     } else {
-
+        return (
+            <div>
+                <p>Too many matches, specify another filter</p>
+            </div>
+        )
     }
 }
 
@@ -35,7 +81,6 @@ const App = () => {
     const handleKeywordChange = (event) => {
         setKeyword(event.target.value);
     }
-
     
     return (
         <div>
